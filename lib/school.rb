e69de98f3f7e71c2b66a08b203ac4243fc02fc8b
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-14 16:17:35 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    # note this instance varialbe IS the hash
+    @roster = {}
+  end
+
+  def name
+    @name = name
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+        @roster[grade] = []
+        @roster[grade] << student
+    else 
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.map do |key, value|
+      value.sort!
+    end 
+    @roster.to_h
+  end
+
+end
